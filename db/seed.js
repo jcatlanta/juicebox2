6244a5834664f99f4e3ce4c5bac846a02ea4ba02
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-01-12 23:00:41 -0500

    Finished Part 3 Creating Tags

--- db/seed.js
+++ db/seed.js
@@ -6,7 +6,9 @@ const { client,
       getAllPosts,
       getPostsByUser,
       createPost,
-      getUserById
+      getUserById,
+      createTags,
+      addTagsToPost
 } = require('./index');
 
 async function dropTables() {
@@ -14,6 +16,8 @@ async function dropTables() {
             console.log("Starting to drop tables...");
 
             await client.query(`
+            DROP TABLE IF EXISTS post_tags;
+            DROP TABLE IF EXISTS tags;
             DROP TABLE IF EXISTS posts;
             DROP TABLE IF EXISTS users;
             `);
@@ -45,6 +49,15 @@ async function createTables() {
                   content TEXT NOT NULL,
                   active BOOLEAN DEFAULT true                 
             );
+            CREATE TABLE tags(
+                  id SERIAL PRIMARY KEY,
+                  name VARCHAR(255) UNIQUE NOT NULL
+            );
+            CREATE TABLE post_tags(
+                  "postId" INTEGER REFERENCES posts(id),
+                  "tagId" INTEGER REFERENCES tags(id),
+                  UNIQUE("postId", "tagId")
+            )
         `);
 
             console.log("Finished building tables!");
@@ -101,6 +114,30 @@ async function createInitialPosts() {
       }
 }
 
+async function createInitialTags() {
+      try {
+            console.log("Starting to create tags...");
+
+            const [happy, sad, inspo, catman] = await createTags([
+                  '#happy',
+                  '#worst-day-ever',
+                  '#youcandoanything',
+                  '#catmandoeverything'
+            ]);
+
+            const [postOne, postTwo, postThree] = await getAllPosts();
+
+            await addTagsToPost(postOne.id, [happy, inspo]);
+            await addTagsToPost(postTwo.id, [sad, inspo]);
+            await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+            console.log("Finished creating tags!");
+      } catch (error) {
+            console.log("Error creating tags!");
+            throw error;
+      }
+}
+
 async function rebuildDB() {
       try {
             client.connect();
@@ -109,6 +146,7 @@ async function rebuildDB() {
             await createTables();
             await createInitialUsers();
             await createInitialPosts();
+            await createInitialTags();
       } catch (error) {
             console.log("Error during rebuildDB");
             throw error;
