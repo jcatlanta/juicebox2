f11877e872232274b48c3515fb5ee06345e07d3a
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-01-18 21:16:37 -0500

    Added middleware

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const server = express();
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
+
+server.use((req, res, next) => {
+      console.log("<____Body Logger START____>");
+      console.log(req.body);
+      console.log("<_____Body Logger END_____>");
+    
+      next();
+    });
